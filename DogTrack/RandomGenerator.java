83086e067e808d716cb86858eef2f5d765eb585e
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-08-27 08:03:18 -0500

    Parte 2

---
+++ DogTrack/RandomGenerator.java
@@ -0,0 +1,13 @@
+package DogTrack;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+	private static Random random=new Random(System.currentTimeMillis());
+	
+	public static int nextInt(int max){
+		return random.nextInt(max);
+	}
+	
+}
